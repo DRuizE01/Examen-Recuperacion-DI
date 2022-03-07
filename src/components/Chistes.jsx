869a633420204da25de51aaf3d0c47172f177307
DRuizE01 <druize01@educantabria.es>
2022-03-07 13:44:01 +0100

    Sin tabla

--- src/components/Chistes.jsx
+++ src/components/Chistes.jsx
@@ -1,73 +1,48 @@
 import React from 'react';
-import {Table} from  'react-bootstrap';
+import { Table } from 'react-bootstrap';
 import Container from 'react-bootstrap/esm/Container';
 import { Card, Container, Table, Row, Col } from 'react-bootstrap';
-import {Card} from 'react-bootstrap';
-import Accordion from 'react-bootstrap/Accordion'
+import { Card } from 'react-bootstrap';
+import Accordion from 'react-bootstrap/Accordion';
 
 class Chistes extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-     selectItem: null,
-     tableData:[]
+      selectItem: null,
+      tableData: [],
     };
   }
 
-  async componentDidMount(){
-    const response =await fetch('https://v2.jokeapi.dev/joke/Any?lang=es&type=twopart&amount=3');
+  async componentDidMount() {
+    const response = await fetch(
+      'https://v2.jokeapi.dev/joke/Any?lang=es&type=twopart&amount=3'
+    );
     const responseData = await response.json();
-  
-    this.setState({tableData : responseData.jokes,
-    });
+
+    this.setState({ tableData: responseData.jokes });
   }
- render(){
-  return (
-    <div className="">
-    <h1>Chistes</h1>
-    <Container>
-      <Row>
-        <Col lg={10} md={6}>
-          <Table responsive striped>
-            <thead>
-              <tr>
-                <th>Pregunta</th>
-                <th>Respuesta</th>
-                
-              </tr>
-            </thead>
-            <tbody>
-            
+  render() {
+    return (
+      <div className="">
+        <h1>Chistes</h1>
 
-
-            {this.state.tableData.map((item) => {
-                    return (
-<Accordion defaultActiveKey="0">
-  <Accordion.Item eventKey="0">
-
-
-  <Accordion.Header>
-    <td>{item.setup}</td>
-  </Accordion.Header>
-    <Accordion.Body>
-      <td>{item.delivery}</td>
-    </Accordion.Body>
-  </Accordion.Item>
-</Accordion>
-                    )
-                  })}
-
-
-            
-           </tbody>
-          </Table>
-        </Col>
-      </Row>
-    </Container>
-  </div>
-  );
-
-
- }
+        {this.state.tableData.map((item) => {
+          return (
+            <Accordion>
+              <Accordion.Item eventKey="0">
+                <Accordion.Header>
+                  <h3>{item.setup}</h3>
+                </Accordion.Header>
+                <Accordion.Body>
+                  <h5>{item.delivery}</h5>
+                </Accordion.Body>
+              </Accordion.Item>
+            </Accordion>
+          );
+        })}
+      </div>
+    );
+  }
 }
- export default Chistes;
+export default Chistes;
